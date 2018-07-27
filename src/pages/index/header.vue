da63bf80155629e0ebf7f73b1d9cbc107e612048
cc <ladyccmost@163.com>
2018-07-27 10:39:40 +0800

    create project

---
+++ src/pages/index/header.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="header">
+        
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "index-header"
+    }
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
